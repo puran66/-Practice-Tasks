7be736b11912d5e8b5603ee171d7907746ad7ee5
Puran Vishwakarma <130432123+puran66@users.noreply.github.com>
2023-06-09 10:02:26 +0530

    Add files via upload

---
+++ 08-06-2023/task2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class Food{
+	private:
+		char rat[30];
+		static char loc[20];
+		int year;
+		int staff;
+	public:
+	    void setter(){
+	    	cout<<"Enter Ratings(1star,2star,3star...) :";
+	    	cin>>rat;
+	    	
+	    	cout<<"Enter Establish Year :";
+	    	cin>>year;
+	    	
+	    	cout<<"Enter staff quantity :";
+	    	cin>>staff;
+		}
+		void getter(){
+			cout<<endl<<"Ratings : "<<rat<<endl<<endl;
+			cout<<"City Nmae : "<<loc<<endl<<endl;
+			cout<<"Establish year :"<<year<<endl<<endl;
+			cout<<"Staff Quantity :"<<staff<<endl<<endl;
+		}
+		
+	    Food(){
+	    	cout<<"Id : 123"<<endl<<endl;
+	    	cout<<"Cafe Name : Max Cafe"<<endl<<endl;
+	    	cout<<"Type : Rooftop"<<endl<<endl;
+		}		
+		
+};
+char Food :: loc[]="Surat";
+
+int main(){
+	Food obj;
+	
+	obj.setter();
+	obj.getter();
+	
+	return 0;
+}
